6db93f1cdd93ab2048c969b2545c80e123b5c530
Diego Cuenca <diegox_cuenca@hotmail.com>
2020-09-20 01:58:48 -0500

    Funciones de crear, editar y listar habitaciones

--- apps/reservas/views.py
+++ apps/reservas/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import *
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hola a todos desde el index de la app reservas.")
+@login_required
+def listar_habitaciones(request, template_name='reservas/listar_habitaciones.html'):
+    lista_habs = Habitacion.objects.all()
+    return render(request, template_name, {'habitaciones':lista_habs})
+
+@method_decorator(login_required, name='dispatch')
+class CrearHabitacion(CreateView):
+    model = Habitacion
+    #fields = ['numero', 'tipo', 'precio', 'planta', 'activa', 'detalles']
+    fields = '__all__'
+    template_name_suffix = '_crear'
+    success_url = reverse_lazy('reservas:listar_hab')
+
+@method_decorator(login_required, name='dispatch')
+class EditarHabitacion(UpdateView):
+    model = Habitacion
+    #fields = ['numero', 'tipo', 'precio', 'planta', 'activa', 'detalles']
+    fields = '__all__'
+    template_name_suffix = '_editar'
+    success_url = reverse_lazy('reservas:listar_hab')
