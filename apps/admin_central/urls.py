9375bba90e741c2faba87fa70d8b3679a86062d3
Diego Cuenca <diegox_cuenca@hotmail.com>
2020-09-25 21:13:51 -0500

    Galería y control panel

--- apps/admin_central/urls.py
+++ apps/admin_central/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #Inicio de la página
+    path('', views.inicio, name='inicio'),
+    path('galeria', views.galeria, name='galeria'),
+
     #Inicio de la Administración
-    path('', views.admin_panel, name='admin_panel'),
+    path('control_panel/', views.control_panel, name='control_panel'),
 ]
--- apps/admin_central/views.py
+++ apps/admin_central/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
+from django.db.models import Sum
 from django.contrib.auth.decorators import login_required
 from apps.reservas.models import Reservacion
 
 # Create your views here.
 def inicio(request):
-    return render(request, 'index.html')   
+    #obtener datos que serán enviados a la vista
+    return render(request, 'index.html')
+
+def galeria(request):
+    #obtener datos que serán enviados a la vista
+    return render(request, 'gallery.html') 
 
 @login_required
-def admin_panel(request, template_name='inicio_admin.html'):
+def control_panel(request, template_name='inicio_admin.html'):
     res = Reservacion.objects.count()
-    return render(request, template_name, {'nro_reservas':res})
+    total_pagos = Reservacion.objects.filter(estado='FN').aggregate(Sum('pago_total'))
+    return render(request, template_name, {'nro_reservas':res, 'total':total_pagos['pago_total__sum']})
 
