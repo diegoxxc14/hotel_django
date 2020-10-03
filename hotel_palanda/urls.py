7b852c47f8f82ea3d3c235e18fd06ba30026bee1
Diego Cuenca <diegox_cuenca@hotmail.com>
2020-10-03 16:39:57 -0500

    Configuración para media

--- hotel_palanda/urls.py
+++ hotel_palanda/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +31,5 @@ urlpatterns = [
     #Aplicación reservas
     path('reservas/', include(('apps.reservas.urls', 'reservas'), namespace='reservas')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
